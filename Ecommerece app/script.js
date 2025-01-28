575ae0c7013bb1a09f6d2bdb28c9c0a4a4ed4344
Gaurav Sharma <gau1711sha@gmail.com>
2025-01-28 19:21:05 +0530

    expense tracker revised

--- Ecommerece app/script.js
+++ Ecommerece app/script.js
@@ -17,65 +17,65 @@ document.addEventListener('DOMContentLoaded',()=>{
  let    checkoutbtn = document.getElementById('checkout-btn');
 
 
- products.forEach((product)=>{
-    let productdiv = document.createElement('div');
-    productdiv.classList.add('product');
-    productdiv.innerHTML = `
-    <span>${product.name} - ${product.price.toFixed(2)}</span>
-    <button data-id="${product.id}">Add to cart</button>
-    `
-    productlist.appendChild(productdiv);
- })
 
- productlist.addEventListener('click',(e)=>{
+products.forEach(product=>{
+  let productdiv = document.createElement('div');
+  productdiv.classList.add('product');
+  productdiv.innerHTML = `
+  <span>${product.name} - $${product.price.toFixed(2)}</span>
+  <button data-id=${product.id}>Add to cart</button>
+  `
+  productlist.appendChild(productdiv);
+})
 
-    if(e.target.tagName =='BUTTON'){
-        const productid = parseInt(e.target.getAttribute('data-id'));
-       let product = products.find(p=>p.id===productid);
-       addtocart(product);
-    }
-
- })
+productlist.addEventListener('click',(e)=>{
+   if(e.target.tagName==='BUTTON'){
+      const productid = parseInt(e.target.getAttribute('data-id'));
+      const product = products.find(p=>p.id===productid);
+      addtocart(product);
+   }
+})
 
 function addtocart(product){
-cart.push(product);
-rendercart(product);
+   cart.push(product);
+   rendercart(product);
 }
 
-function rendercart(product){
-   cartitems.innerText = "";
-   let totalprice = 0;
+function rendercart(){
+           cartitems.innerText = "";
+            let totalprice = 0;
 
-   if(cart.length>0){
-      emptycartmsg.classList.add('hidden');
-           carttotalmsg.classList.remove('hidden');
+            if(cart.length>0){
+               emptycartmsg.classList.add('hidden');
+               carttotalmsg.classList.remove('hidden');
 
 cart.forEach((item,index)=>{
    totalprice+=item.price;
-      let renderdiv = document.createElement('div');
-    renderdiv.innerHTML = `
-    <span>${item.name}-${item.price}</span>
-    `
-    cartitems.appendChild(renderdiv);
-    totalpricedisplay.textContent = `${totalprice.toFixed(2)}`
+   const renderdiv = document.createElement('div');
+   renderdiv.innerHTML=`
+   <span>${item.name}-$${item.price}</span>
+   `
+   cartitems.appendChild(renderdiv);
+   totalpricedisplay.textContent = `${totalprice.toFixed(2)}`
 })
 
-
-   }else{
-      emptycartmsg.classList.remove('hidden');
+    }else{
+          emptycartmsg.classList.remove('hidden');
       cartitems.innerHTML= ' ';
       totalpricedisplay.textContent= `$0.00`
-   }
-   
+            }
 }
 
-checkoutbtn.addEventListener('click', ()=>{
-   cart.length = 0;
+checkoutbtn.addEventListener('click',()=>{
+  
+      cart.length = 0;
    alert("checked out successfully")
    rendercart();
 })
 
 
 
-
  })
+
+
+
